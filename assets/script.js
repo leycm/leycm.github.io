0d5fad7e88f08a5948d053be11878e94abf2bf92
notTheLey <cm.ley.cm@gmail.com>
2025-02-24 02:02:30 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log('Zeige Elemente mit der Klasse "needcookies".');
     needCookiesElements.forEach(element => {
       element.style.removeProperty( 'display' );
-      element.style.display = null;
+      element.style.display = block;
       element.style.visibility = true;
     });
   }
